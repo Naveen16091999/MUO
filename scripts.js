947539cb80e15755d8bc235cc39d5e77138f1e43
Naveen Kumar <146556539+Naveen16091999@users.noreply.github.com>
2024-10-21 11:16:14 +0530

    style.css

---
+++ scripts.js
@@ -0,0 +1,30 @@
+/*
+
+
+
+// Side menu toggle
+document.getElementById('side_menu').addEventListener('change', function() {
+    let sideNav = document.getElementById('side-navigation');
+    if (this.checked) {
+        sideNav.classList.add('is-open');
+    } else {
+        sideNav.classList.remove('is-open');
+    }
+});
+
+// Search menu toggle
+document.getElementById('search-menu').addEventListener('change', function() {
+    let searchBox = document.querySelector('.header-search-content');
+    if (this.checked) {
+        searchBox.style.display = 'block';
+    } else {
+        searchBox.style.display = 'none';
+    }
+});
+
+// Close search on clicking close button
+document.querySelector('.header-search-exit').addEventListener('click', function() {
+    document.getElementById('search-menu').checked = false;
+    document.querySelector('.header-search-content').style.display = 'none';
+});
+*/
